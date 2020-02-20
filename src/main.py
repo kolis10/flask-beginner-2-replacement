d060dfa123adee1f66d9783a69a122d0f9768591
kolis10 <issacalleyne@gmail.com>
2020-02-20 20:06:43 +0000

    first commit

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
+from models import db, Jobs, Users, Achievements
 #from models import Person
 
 app = Flask(__name__)
@@ -27,6 +27,96 @@ def handle_invalid_usage(error):
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
+@app.route('/register', methods=['POST'])
+def handle_login():
+
+    json = request.get_json()
+
+    db.session.add(Users(
+        first_name = json['first_name'],
+        last_name = json['last_name'],
+        username = json['username'],
+        date_of_birth = json['date_of_birth'],
+        email = json['email']
+    ))
+    db.session.commit()
+    return jsonify(json)
+
+@app.route('/users')
+def get_user():
+
+    return jsonify( Users.query.get(7).serialize() )
+
+@app.route('/delete')
+def delete():
+
+    db.session.delete(Achievements.query.get())
+    db.session.commit()
+    return 'done'
+    
+    
+@app.route('/achievements', methods=['POST'])
+def achievements():
+
+    json = request.get_json()
+
+    db.session.add(Achievements(
+        user_id = json['user_id'],
+        achievement = json['achievement'],
+        reward = json['reward']
+    ))
+    db.session.commit()
+    return jsonify(json)
+
+@app.route('/change', methods=['POST'])
+def handle_change():
+
+    json = request.get_json()
+    member = Users.query.filter_by(json["first_name"]).first()
+    member_dict = member.serialize()
+    if member is None:
+        return 'User Not Found: 404'
+    
+    return jsonify(member_dict)
+
+@app.route('/ex_1', methods=['POST'])
+def handle_ex_1():
+
+    json = request.get_json()
+
+    db.session.add(Jobs(
+        job_name = json['name'],
+        job_place = json['place'],
+        job_pay = json['pay']
+    ))
+    db.session.commit()
+    return jsonify(json)
+
+@app.route('/ex_2', methods=['GET'])
+def handle_ex_2():
+
+    employ = Jobs.query.get(5)
+    employ_dict = employ.serialize()
+    
+    return jsonify(employ_dict)
+
+
+
+@app.route('/got_it', methods=['POST', 'GET'])
+def handle_got_it():
+
+    return 'got it'
+
+@app.route('/all_odd', methods=['POST', 'GET'])
+def handle_all_odd():
+    i = 0
+    all_odds = []
+    while i < 20:
+        i += 1
+        if i %2 != 0:
+            all_odds.append(i)
+    return jsonify(all_odds)
 
 @app.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
--- src/models.py
+++ src/models.py
@@ -2,6 +2,68 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Jobs(db.Model):
+    __tablename__ = 'jobs'
+    id = db.Column(db.Integer, primary_key=True)
+    job_name = db.Column(db.String(50))
+    job_place = db.Column(db.String(50))
+    job_pay = db.Column(db.String(20))
+
+    def __repr__(self):
+        return '<Jobs %r>' % self.username
+
+    def serialize(self):
+        return {
+            "job_name": self.job_name,
+            "job_place": self.job_place,
+            "job_pay": self.job_pay
+        }
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(40))
+    last_name = db.Column(db.String(40))
+    username = db.Column(db.String(40))
+    date_of_birth = db.Column(db.String(10))
+    email = db.Column(db.String(120))
+
+    achievements = db.relationship('Achievements', back_populates='user')
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "first name": self.first_name,
+            "last name": self.last_name,
+            "username": self.username,
+            "date of birth": self.date_of_birth,
+            "email": self.email,
+            "achievements": [x.serialize() for x in self.achievements]
+        }
+
+class Achievements(db.Model):
+    __tablename__ = 'achievements'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    achievement = db.Column(db.String(100))
+    reward = db.Column(db.String(40))
+    
+    user = db.relationship('Users', back_populates='achievements')
+
+    def __repr__(self):
+        return '<Achievements %r>' % self.achievement
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "achievement": self.achievement,
+            "username": self.user.username,
+            "reward": self.reward
+        }
+
 # class Person(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
 #     username = db.Column(db.String(80), unique=True, nullable=False)
